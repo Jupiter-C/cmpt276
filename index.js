a2e321bf66a90659904a6ad883fcad0e604aa0d0
Claire Jupiter <jupiteryao@icloud.com>
2019-10-14 18:45:50 -0700

    tokimmm

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const pool = new Pool({
   ssl: true
 });
 
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -12,8 +13,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => {res.render('pages/index')});
-  
+  .get('/', (req, res) => {res.render('pages/index')})
+
   .post('/create', (req, res) => {
     var name = req.body.name;
     var weight = req.body.weight;
@@ -26,16 +27,16 @@ express()
     var frozen = req.body.frozen;
     var total = req.body.total;
     var trainnername = req.body.trainnername;
-    
+
     var  insertUsersQuery = `INSERT INTO tokimon VALUES ('${name}', ${weight}, ${height}, ${fly}, ${fight}, ${fire}, ${water}, ${electric}, ${frozen}, ${total}, '${trainnername}');`
     console.log(insertUsersQuery);
     pool.query(insertUserQuery, (error,result) => {
       if (error)
         res.end(error);
-        var results = {'rows': result.rows };
-        console.log(results);
+      var results = {'rows': result.rows };
+      console.log(results);
     res.render('pages/users', results)
     });
   })
-  
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
